9626346b7c88f5c4fdf0a9c34a95d685223f41f9
qiangShan <1059931090@qq.com>
2023-03-12 17:59:09 +0800

    添加键盘监听事件

--- src/main/java/com/mashibing/tank/TankFrame.java
+++ src/main/java/com/mashibing/tank/TankFrame.java
@@ -1,16 +1,22 @@
 package com.mashibing.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+
+    int x=200,y=200;
 
     public TankFrame(){
         this.setSize(960,720);
         this.setResizable(false);
         this.setTitle("tank_battle");
         this.setVisible(true);
+
+        this.addKeyListener(new MyKeyListener());
 
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -22,6 +28,21 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g){
-        g.fillRect(200,200,50,50);
+        g.fillRect(x,y,50,50);
+        x += 10;
+        y += 10;
+    }
+
+    class MyKeyListener extends KeyAdapter{
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
     }
 }
